d4afa268d00366fe292bf6713b0295299c565a01
Prashank <prashankm5@gmail.com>
2024-02-26 00:47:51 +0530

    vendor profile form + validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,10 @@
+from typing import Any, Mapping
 from django import forms
-from .models import User
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -19,3 +24,24 @@ class UserForm(forms.ModelForm):
         if password != confirm_password:
             raise forms.ValidationError('Password does not Match!')
     
+
+
+class UserProfileForm(forms.ModelForm):
+
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Start typing .....", "required": "required",}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={"class":"btn btn-info"}), validators=[allow_only_images_validator])
+    cover_picture = forms.FileField(widget=forms.FileInput(attrs={"class": "btn btn-info"}), validators=[allow_only_images_validator])
+
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={"readonly": "readonly"}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={"readonly": "readonly"}))
+    class Meta:
+        model = UserProfile
+        fields = ('profile_picture', 'cover_picture', 'address', 'country', 'state' ,'city' , 'pin_code', 'latitude', 'longitude')
+
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            if field =='latitude' or field =='longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
--- accounts/models.py
+++ accounts/models.py
@@ -101,8 +101,8 @@ class UserProfile(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='users/profile_picture', blank=True, null=True)
     cover_picture = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
-    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
-    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    # address_line_2 = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
@@ -118,6 +118,6 @@ class UserProfile(models.Model):
         return self.user.email
 
 
-    def full_address(self):
-        return f"{self.address_line_1}, {self.address_line_2}"
+    # def full_address(self):
+    #     return f"{self.address_line_1}, {self.address_line_2}"
 
